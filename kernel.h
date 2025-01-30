ba771ab76f7ee0817a6a2f285606cbf9e6129d70
Yaro Schiffelers <yaroschiffelers@users.noreply.github.com>
2025-01-30 22:59:16 +0100

    chore: adds explaination and specs reference

--- kernel.h
+++ kernel.h
@@ -2,6 +2,7 @@
 
 // SBI functions must return a pair of values in a0 and a1,
 // with a0 returning an error code.
+// @ref RISC-V Supervisor Binary Interface Specification - v2.0-rc1
 struct sbiret
 {
     long error;
