3b9236cc4b4a610f38827b6d2b4cc1a18d5cb0dd
Yaro Schiffelers <yaroschiffelers@users.noreply.github.com>
2025-02-04 16:30:51 +0100

    chore: adds comments

--- common.h
+++ common.h
@@ -6,19 +6,25 @@ typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
 typedef uint32_t size_t;
-typedef uint32_t paddr_t;
-typedef uint32_t vaddr_t;
+typedef uint32_t paddr_t; // physical memory address
+typedef uint32_t vaddr_t; // virtual memory address (equivalent to uintptr_t)
 
 #define true 1
 #define false 0
 #define NULL ((void *)0)
-#define align_up(value, align) __builtin_align_up(value, align)
-#define is_aligned(value, align) __builtin_is_aligned(value, align)
-#define offsetof(type, member) __builtin_offsetof(type, member)
 #define va_list __builtin_va_list
 #define va_start __builtin_va_start
 #define va_end __builtin_va_end
 #define va_arg __builtin_va_arg
+
+// rounds value to nearest multiple of align. Align must be a power of 2
+#define align_up(value, align) __builtin_align_up(value, align)
+
+// checks if value is a multiple of align. Align must be a power of 2
+#define is_aligned(value, align) __builtin_is_aligned(value, align)
+
+// returns the offset of a member within a structure (x amount of bytes from start)
+#define offsetof(type, member) __builtin_offsetof(type, member)
 
 void *memset(void *buf, char c, size_t n);
 void *memcpy(void *dst, const void *src, size_t n);
